b88535553a3aba95ef3aec888676f8f35b64f633
osparking <jbpark03@gmail.com>
2023-09-04 23:01:51 +0900

    just spring context created

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import config.ProjectConfig;
+
+public class Main {
+
+	public static void main(String[] args) {
+		var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+		
+	}
+
+}
