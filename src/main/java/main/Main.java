62e6510c9231b57d9ea464ac5d6f38fe57825650
osparking <jbpark03@gmail.com>
2023-09-05 04:49:29 +0900

    fetch commentservice bean

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,14 +1,22 @@
 package main;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.PrintStream;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import beans.CommentService;
 import config.ProjectConfig;
 
 public class Main {
 
 	public static void main(String[] args) {
 		var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
-		
+		var ps = new PrintStream(System.out, true, UTF_8);
+		ps.println("채취 전");
+		var sv = c.getBean(CommentService.class);
+		ps.println("채취 후");
 	}
 
 }
