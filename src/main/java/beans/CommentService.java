01101038f9a72a96e8e10b1ebb7e3406d9a3c0bd
osparking <jbpark03@gmail.com>
2023-09-04 22:57:52 +0900

    message that shows when bean instentiated

---
+++ src/main/java/beans/CommentService.java
@@ -0,0 +1,10 @@
+package beans;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+	public CommentService() {
+		System.out.println("commentservice instance created");
+	}
+}
